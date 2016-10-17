f55ebe803f0865d88ca7a5ff7a7e88ac3bbb5bd0
Kevin Julián Martínez Escobar <kevinccbsg@gmail.com>
2016-10-17 15:11:00 +0200

    Updated mongo gridfs

--- app_gridfs.js
+++ app_gridfs.js
@@ -17,6 +17,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/uploads', express.static(__dirname +'/uploads'));
 //app.use(multer({dest: './uploads/'}))
 
@@ -24,10 +31,15 @@ app.post('/upload', function(req, res, next) {
 	var form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files) {
+        console.log(fields);
+        console.log(files);
     	var dirname = require('path').dirname(__dirname);
         var filename = files.files.name;
 		var path = files.files.path;
 		var type = files.files.type;
+        var lifnr = fields.lifnr;
+        var system = fields.system;
+        var subsystem = fields.subsystem;
 
 		var read_stream =  fs.createReadStream(path);
 
@@ -37,31 +49,46 @@ app.post('/upload', function(req, res, next) {
 		var gfs = Grid(conn.db);
 
 		var writestream = gfs.createWriteStream({
-			filename: filename
+			filename: filename,
+            content_type: type,
+            metadata: {
+                lifnr: lifnr,
+                system: system,
+                subsystem: subsystem
+            }
 		});
 		read_stream.pipe(writestream);
+        res.json({ filename: filename });
 		console.log(files.files.name);
 		next();
     });
 	/*
 	*/
 });
-
-app.get('/uploads/:filename', function (req, res, next) {
-      var gfs = Grid(conn.db);
-      var filename = req.params.filename + '.jpg';
- 		var writtable = concatStream();
-       gfs.files.find({}).toArray(function (err, files) {
- 		
- 		console.log(files.type);
+app.get('/uploads', function (req, res, next) {
+    var gfs = Grid(conn.db);
+    var filename = req.query.filename;
+    var lifnr = req.query.lifnr;
+    var system = req.query.system;
+    var subsystem = req.query.subsystem;
+    var jsonSearch = {
+        metadata: {
+            lifnr: lifnr,
+            system: system,
+            subsystem: subsystem
+        }
+    };
+    gfs.files.find(jsonSearch).toArray(function (err, files) {
+		
+    	console.log(files);
         if (err) {
             res.json(err);
         }
-        if (files.length > 0) {
-    		var mime = 'image/jpeg';
-            res.set('Content-Type', mime);
+        if (files) {
             var read_stream = gfs.createReadStream({filename: filename});
             read_stream.pipe(res);
+            console.log(typeof(read_stream));
+            console.log(read_stream);
         } else {
             res.json('File Not Found');
         }
