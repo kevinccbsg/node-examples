26e21e78c13f4684173f6d046f6999e6267efa75
kevinccbsg <kevin2906@hotmail.com>
2017-01-12 21:27:09 +0100

    azusermanagment tests

--- frontBack-Angular/app.js
+++ frontBack-Angular/app.js
@@ -1,15 +1,30 @@
 'use strict'
 var express = require('express');
-//var path = require('path');
+var path = require('path');
 var app = express();
+var azuserManagerClient = require('azuser-manager-client');
+
+var client = azuserManagerClient.createClient({
+  uri: 'http://localhost:3001',
+  headers: {
+    'Content-type': 'application/json'
+  }
+});
+app.use('/', express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug');
 
 
-/*app.use('/', express.static(path.join(__dirname, 'public')))
-app.set('view engine', 'pug');*/
-
-app.get('/', function (req, res) {
-  console.log('ey');
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+app.get('/user/:id', function(req, res) {
+  res.json({
+    cool: true
+  });
+});
+app.get('/test-send', function (req, res) {
+  console.log(client.headers);
+  client.getUser('id', function(err, user) {
+    res.send(user);
+  });
+  
 });
 
 app.listen(3001, function () {
